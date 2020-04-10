70bb429f42158c734530510434695103663eb30f
salfaizal <Faizalardiansyah3@gmail.com>
2020-04-10 10:16:23 +0800

    memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+	public static void tampilanInfo(){
+		System.out.println();
+		System.out.println("== Data Mahasiswa ===================");
+		System.out.println("NPM : 16630762");
+		System.out.println("Nama : Mochamad Nurfaizal Ardiansyah");
+		System.out.println("=====================================");
+	}
+}
